2868f6c1a251d1975830cb83ea62f79d61c68e9c
Reginaldo Luiz Formoso Junior <formosorl@yahoo.com.br>
2018-03-27 18:02:14 -0300

    Add files via upload

---
+++ Hello World!/Program1.cbl
@@ -0,0 +1,23 @@
+       IDENTIFICATION DIVISION.
+       program-id. Program1 as "DISPLAY FATEC".
+       AUTHOR.     REGINALDO_FATEC. 
+       
+       environment division.
+       configuration section.
+
+       data division.
+       working-storage section.
+       
+      *****************************************
+      *DISPLAY FATEC
+      *****************************************
+      
+       procedure division.
+       
+           DISPLAY "FATEC   FATEC  FATEC". 
+
+           STOP RUN.
+           
+             goback.
+
+       end program Program1.
